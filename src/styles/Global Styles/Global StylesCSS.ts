fdaeee249cd92d85646c3703ab865145ea257d7e
annekarolle <102996739+annekarolle@users.noreply.github.com>
2022-08-30 15:18:07 -0300

    feat / ticke-005 implementando root color

--- src/styles/Global Styles/Global StylesCSS.ts
+++ src/styles/Global Styles/Global StylesCSS.ts
@@ -1,6 +1,16 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+   :root {
+    --color-green: #00A48B;
+    --color-green2: #6FEFD2;
+    --color-green3: #0B362F;
+    --color-white: #FFFFFF;
+    --color-grey: #F2F2F2;
+    --color-transparencia: #99A8A5;
+   
+    font-size: 60%;   
+  }
   * {
     margin: 0;
     padding: 0;
