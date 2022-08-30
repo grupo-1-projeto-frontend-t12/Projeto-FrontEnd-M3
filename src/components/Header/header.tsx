c95081412215bf7bdc7e14b74f03a0831040b51f
Daniel Galvan <dgalvan@gmail.com>
2022-08-30 20:24:06 -0300

    feat: ticket_015 e ticket_025 - implementaçãio header, ajuste css menu e logo

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,14 +1,23 @@
 import Menu from "../menu/menu";
-import Logo from "../../assets/img/logo/KenzieMED.png"
+import Logo from "../../assets/img/logo/KenzieMED (1).svg";
+import Medica from "../../assets/img/logo/medica.svg"
 import { TagHeader } from "./headerCSS";
+import ButtonAppointment from "../ButtonAppointment/buttonAppointment";
 
 const Header = () => {
   return (
     <TagHeader>
-      <div>
+      <div className="container">
         <img src={Logo} alt="Logo KenzieMED" />
-        <Menu />
+        <div className="container-buttons">
+          <Menu />
+          <ButtonAppointment />
+        </div>
       </div>
+        <div className="container-info">
+          <img src={Medica} alt="Medica" />
+          <h2>Faça seu agendamento rápido e fácil</h2>
+        </div>
     </TagHeader>
   );
 };
--- src/components/Header/headerCSS.ts
+++ src/components/Header/headerCSS.ts
@@ -5,22 +5,80 @@ export const TagHeader = styled.header`
   justify-content: center;
   width: 100%;
   height: 415px;
-  background-color: var(--color-green2);
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  background: var(--bg-color-linear);
 
-  > div {
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  position: relative;
+
+  > .container {
     width: 95%;
-    height: 65px;
+    height: 375px;
     max-width: 1200px;
-    margin-top: 60px;
+    margin-top: 50px;
     display: flex;
+    flex-direction: column;
     justify-content: space-between;
     align-items: center;
+    z-index: 1;
+
+    @media screen and (min-width: 768px) {
+      flex-direction: row;
+      height: 48px;
+    }
 
     > img {
       width: 188px;
       height: 32px;
     }
+
+    .container-buttons {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      align-items: center;
+      height: 290px;
+      gap: 15px;
+
+      @media screen and (min-width: 768px) {
+        flex-direction: row;
+        height: 48px;
+      }
+    }
   }
 
+  .container-info {
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    height: 68%;
+
+    @media screen and (min-width: 768px) {
+      position: absolute;
+      align-items: center;
+      height: 100%;
+    
+    }
+
+    > img {
+      scale: .5;
+
+      @media screen and (min-width: 768px) {
+        scale: 1;
+        object-fit: scale-down;
+      }
+    }
+    
+
+    > h2 {
+      display: none;
+      font-size: 36px;
+      color: var(--color-green3);
+      width: 226px;
+
+      @media screen and (min-width: 768px) {
+        display: block;
+      }
+    }
+
+  }
 `;
