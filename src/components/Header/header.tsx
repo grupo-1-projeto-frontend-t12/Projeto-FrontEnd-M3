1b47e4e1eaa233cb88f57ed2ddb2c890775af403
annekarolle <102996739+annekarolle@users.noreply.github.com>
2022-08-30 13:59:10 -0300

    teste

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,28 +1,11 @@
-<<<<<<< HEAD
-import { useContext } from "react"
-import { UserContext } from "../../context/authContext";
-import { TagHeader } from "./headerCSS"
-=======
 import { useContext } from "react";
 import { UserContext } from "../../context/AuthContext";
 import { TagHeader } from "./headerCSS";
->>>>>>> 9435d055e2528e35a0e5fb6e0f4a8ccc54b2dfae
 
 const Header = () => {
   const { login, setLogin } = useContext(UserContext);
   console.log("LOG DO LOGIN ", login); // APAGAR DEPOIS
 
-<<<<<<< HEAD
-    const { login, setLogin } = useContext(UserContext)
-    console.log("LOG DO LOGIN ",login) // APAGAR DEPOIS 
-
-    return (
-        <TagHeader>
-           
-        </TagHeader>
-    )
-}
-=======
   return (
     <TagHeader>
       <button type="button" onClick={() => setLogin(!login)}>
@@ -31,6 +14,5 @@ const Header = () => {
     </TagHeader>
   );
 };
->>>>>>> 9435d055e2528e35a0e5fb6e0f4a8ccc54b2dfae
 
 export default Header;
--- src/components/Header/headerCSS.ts
+++ src/components/Header/headerCSS.ts
@@ -1,19 +1,10 @@
 import styled from "styled-components";
 
-<<<<<<< HEAD
-export const TagHeader = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-=======
 export const TagHeader = styled.header`
   display: flex;
   justify-content: center;
   align-items: center;
->>>>>>> 9435d055e2528e35a0e5fb6e0f4a8ccc54b2dfae
-
   width: 100%;
   height: 50px;
-
   background: orange;
 `;
