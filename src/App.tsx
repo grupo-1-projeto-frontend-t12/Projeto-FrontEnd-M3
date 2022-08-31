b4117ce58e26d6f29ac4ac2ad2128ce2ef86fb84
annekarolle <102996739+annekarolle@users.noreply.github.com>
2022-08-31 15:21:54 -0300

    feat: ticket 029 - Composição da page Home

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import { AppDiv } from "./styles/appCSS/AppCSS";
 import Header from "./components/Header/header";
 import Footer from "./components/Footer/footer";
 import CardScheduleDoctor from "./components/CardScheduleDoctors/cardScheduleDoctors";
+import Home from "./pages/dashboard";
 
 function App() {
   return (
       <AppDiv>
-        <Header />
-        <CardScheduleDoctor/>
-        <Footer />
+        <Home/>
       </AppDiv>
   );
 }
