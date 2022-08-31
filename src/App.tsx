8352b5202797dd9c8daf7d3c301d3996a9a10c7f
Daniel Galvan <dgalvan@gmail.com>
2022-08-31 10:48:13 -0300

    refactor: ajuste context CardDoctors

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ function App() {
   return (
       <AppDiv>
         <Header />
-        
         <Footer />
       </AppDiv>
   );
--- src/components/CardDoctors/cardDoctors.tsx
+++ src/components/CardDoctors/cardDoctors.tsx
@@ -1,32 +1,32 @@
+import { ContainerDoctors } from "./cardDoctorsStyle";
+import { BsCalendar3 } from "react-icons/bs";
+import { MdOutlinePlace } from "react-icons/md";
+import { useContext, useState } from "react";
+import AuthContext from "../../context/AuthContext";
 
-import { ContainerDoctors } from "./cardDoctorsStyle"
-import {BsCalendar3} from "react-icons/bs"
-import {MdOutlinePlace} from "react-icons/md"
-import { useContext, useState } from "react"
-import { UserContext } from "../../context/AuthContext"
+const {doctorsList} = useContext(AuthContext)
 
-const {doctorsList} = useContext(UserContext)
+const CardDoctors = () => {
+  return (
+    <ContainerDoctors>
+      {doctorsList.map((doctor) => (
+        <>
+          <div className="containerHeader">
+            <h2>{doctor.name}</h2>
+            <button>
+              <BsCalendar3 />
+            </button>
+          </div>
+          <span>{doctor.CRM}</span>
+          <h3>{doctor.speciality}</h3>
+          <h3>
+            <MdOutlinePlace />
+            Endereço
+          </h3>
+        </>
+      ))}
+    </ContainerDoctors>
+  );
+};
 
-const CardDoctors = () =>{
-
-        return (
-            <ContainerDoctors>
-                {
-                doctorsList.map((doctor) =>
-                <>                
-                <div className="containerHeader">
-                   <h2>doctor.name</h2> 
-                   <button><BsCalendar3/></button>
-                </div>  
-                   <span>doctor.CRM</span>
-                   <h3>doctor.speciality</h3>
-                   <h3><MdOutlinePlace/>Endereço</h3>
-               </>
-                )
-                }
-            </ContainerDoctors>
-        )
-
-}
-
-export default CardDoctors
+export default CardDoctors;
