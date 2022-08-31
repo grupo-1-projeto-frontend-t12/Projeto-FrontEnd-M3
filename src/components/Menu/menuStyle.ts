ce6d3c7336a0a172b278a888f35a4a1cddcd8188
Daniel Galvan <dgalvan@gmail.com>
2022-08-30 22:39:54 -0300

    fix: correção pasta component Menu

---
+++ src/components/Menu/menuStyle.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const ContainerMenu = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 280px;
+  height: 38px;
+  background: var(--color-grey);
+  color: var(--color-fontescura);
+  gap: 10px;
+  border-radius: 25px;
+  font-size: 12px;
+  font-weight: 500;
+  color: var(--color-green3);
+  button {
+    border-radius: 25px;
+    border: none;
+    width: 100px;
+  }
+  button:focus {
+    background-color: var(--color-green);
+    color: var(--color-white);
+    height: 100%;
+    border-radius: 25px;
+  }
+`;
