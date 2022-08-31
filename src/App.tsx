d3f2bc0df2b01b00789c37ffb78ff04b4221ded4
annekarolle <102996739+annekarolle@users.noreply.github.com>
2022-08-31 09:55:42 -0300

    feat: ticket 017 - Criação do card e do map do doctor list. Alteração da estilização do global, appDiv e cores do botão.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { AppDiv } from "./styles/appCSS/AppCSS";
 import Header from "./components/Header/header";
 import Footer from "./components/Footer/footer";
-import CardDoctors from "./components/CardDoctors/cardDoctors";
 
 function App() {
   return (
