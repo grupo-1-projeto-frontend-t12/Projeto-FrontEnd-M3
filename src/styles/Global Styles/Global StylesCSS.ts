4b8054bdc7f8dd24cfd1bda7e9f38caf07dbc109
Daniel Galvan <dgalvan@gmail.com>
2022-08-31 22:57:56 -0300

    fix: ticket024 - ajuste espaços GlobalStyle

--- src/styles/Global Styles/Global StylesCSS.ts
+++ src/styles/Global Styles/Global StylesCSS.ts
@@ -16,6 +16,7 @@ const GlobalStyle = createGlobalStyle`
 
     font-family: "Inter", sans-serif;
   }
+
   * {
     margin: 0;
     padding: 0;
@@ -23,6 +24,7 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
    
   }
+
   body{   
     width: 100vw;
     height: 100vh;      
