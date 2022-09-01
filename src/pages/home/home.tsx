291a4b805ebf73b3816c8735b0d8b971c16d7ff9
Frederico Costa <costafredericob@gmail.com>
2022-09-01 14:10:14 -0300

    feat/animations transition home-login implementada

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,7 @@
 import Carousel from "../../components/Carousel/carousel";
 import Footer from "../../components/Footer/footer";
 import Header from "../../components/Header/header";
-
+import { motion } from "framer-motion";
 const Home = () => {
   return (
     <>
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -3,16 +3,19 @@ import CorpoClinico from "../pages/CorpoClinico/corpoClinico";
 import Home from "../pages/Home/home";
 import Login from "../pages/Login/login";
 import Register from "../pages/Register/Register";
+import { AnimatePresence } from "framer-motion";
 
 const AllRoutes = () => {
   return (
-    <Routes>
-      <Route path="*" element={<Navigate to="/" />} />
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/corpoclinico" element={<CorpoClinico />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <AnimatePresence>
+      <Routes>
+        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/corpoclinico" element={<CorpoClinico />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </AnimatePresence>
   );
 };
 
