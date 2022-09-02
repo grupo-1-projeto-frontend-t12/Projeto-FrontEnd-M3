abbf60031ab98a33d1649df79ed19b29dfcc0772
Frederico Costa <costafredericob@gmail.com>
2022-09-02 14:47:41 -0300

    feat/agendamentos - base  da função criada por mim, Rook, Marcus

--- src/components/CardScheduleDoctors/cardScheduleDoctors.tsx
+++ src/components/CardScheduleDoctors/cardScheduleDoctors.tsx
@@ -1,15 +1,53 @@
 import { useContext } from "react";
-import { ContainerRenderDoctorSchedule, ContainerSchedule } from "./cardScheduleDoctorStyle";
-import { AiOutlineClockCircle } from "react-icons/ai";
+import {
+  ContainerRenderDoctorSchedule,
+  ContainerSchedule,
+} from "./cardScheduleDoctorStyle";
+import { AiFillPicture, AiOutlineClockCircle } from "react-icons/ai";
 import { BsCalendar3 } from "react-icons/bs";
 import { AiOutlinePlusCircle } from "react-icons/ai";
 import { AuthContext } from "../../context/AuthContext";
 import { BsArrowLeftShort } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
+import api from "../../services/api";
+import { IDoctorSchedule } from "../../interface/IDoctorSchedule";
 
 const CardScheduleDoctor = () => {
-  const { doctorSchedule, doctor } = useContext(AuthContext);
+  const { doctorSchedule, setDoctorSchedule, doctor, user } =
+    useContext(AuthContext);
   const navigate = useNavigate();
+
+  async function postAppointment(
+    data: IUserAppointment,
+    schedule: IDoctorSchedule
+  ) {
+    const response = await api
+      .post(`/appointment`, data)
+      .then((res) => {
+        setDoctorSchedule(
+          doctorSchedule.filter((horario) => horario.id !== schedule.id)
+        );
+        const resposta = api
+          .patch(`/doctors/${doctor.id}`, doctorSchedule)
+          .then()
+          .catch((error) => {
+            console.warn(error);
+          });
+      })
+      .catch((error) => {
+        console.warn(error);
+      });
+  }
+
+  function agendamento(schedule: IDoctorSchedule): void {
+    const pacote: IUserAppointment = {
+      userId: user.id,
+      doctor: doctor.id,
+      dia: schedule.dia,
+      horario: schedule.hora,
+    };
+    postAppointment(pacote, schedule);
+  }
 
   return (
     <ContainerRenderDoctorSchedule>
@@ -32,17 +70,17 @@ const CardScheduleDoctor = () => {
       <div className="containerSchedule">
         <ContainerSchedule>
           <ul>
-            {doctorSchedule.map((doctor) => (
-              <li key={doctor.id}>
+            {doctorSchedule.map((schedule) => (
+              <li key={schedule.id}>
                 <div className="containerList">
                   <BsCalendar3 />
-                  <h3>{doctor.dia}</h3>
+                  <h3>{schedule.dia}</h3>
                 </div>
                 <div className="containerList">
                   <AiOutlineClockCircle />
-                  <h3>{doctor.hora}</h3>
+                  <h3>{schedule.hora}</h3>
                 </div>
-                <button type="button">
+                <button type="button" onClick={agendamento(schedule)}>
                   <AiOutlinePlusCircle />
                 </button>
               </li>
